4ed0b3b535c5cdb151ffbe4eac0c1d0cd30e7c88
jamie7dev <jamie7dev@gmail.com>
2022-12-14 17:17:52 +0900

    Feat: "추가 입력창, 작성하기 버튼"
    일기장 기본 개발

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -3,7 +3,8 @@ import { useState } from "react";
 const DiaryEditor = () => {
   const [state, setState] = useState({
     author: "",
-    content: ""
+    content: "",
+    emotion: 1,
   });
 
   const handleChangeState = (e) => {
@@ -11,6 +12,11 @@ const DiaryEditor = () => {
       ...state,
       [e.target.name]: e.target.value,
     });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장 성공");
   };
 
   return(
@@ -29,6 +35,22 @@ const DiaryEditor = () => {
           value={state.content}
           onChange={handleChangeState}
         />
+      </div>
+      <div>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );
