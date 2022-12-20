a564c4cf103d457773f0bb59c5c5ebd69c50f8a9
jamie7dev <jamie7dev@gmail.com>
2022-12-20 17:29:07 +0900

    Feat: "추가 프로젝트 전체 최적화"

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,6 +1,17 @@
-import React, {useRef, useState} from "react";
+import React, { useEffect, useRef, useState } from 'react';
 
-const DiaryItem = ({ author, content, emotion, created_date, id, onRemove, onEdit }) => {
+const DiaryItem = ({
+  author,
+  content,
+  emotion,
+  created_date,
+  id,
+  onRemove,
+  onEdit,
+}) => {
+  useEffect(() => {
+    console.log(`${id}번째 아이템 렌더`);
+  });
 
   const [isEdit, setIsEdit] = useState(false);
   const toggleIsEdit = () => setIsEdit(!isEdit);
@@ -10,7 +21,7 @@ const DiaryItem = ({ author, content, emotion, created_date, id, onRemove, onEdi
   const localContentInput = useRef();
 
   const handleRemove = () => {
-    if(window.confirm(`${id}번 일기를 정말 삭제하시겠습니까?`)){
+    if (window.confirm(`${id}번 일기를 정말 삭제하시겠습니까?`)) {
       onRemove(id);
     }
   };
@@ -23,53 +34,52 @@ const DiaryItem = ({ author, content, emotion, created_date, id, onRemove, onEdi
 
   // 수정 완료 버튼 클릭 시 내용 반영되는 함수
   const handleEdit = () => {
-    if(localContent.length < 2) {
+    if (localContent.length < 2) {
       localContentInput.current.focus();
       return;
     }
 
-    if (window.confirm(`${id}번 일기를 수정하시겠습니까?`)){
+    if (window.confirm(`${id}번 일기를 수정하시겠습니까?`)) {
       onEdit(id, localContent);
       toggleIsEdit();
     }
   };
 
-
   return (
     <div className="DiaryItem">
-    <div className="info">
-      <span>작성자 : {author} | 감정점수 : {emotion}</span>
-      <br />
-      <span className="date">{new Date(created_date).toLocaleString()}</span>
-    </div>
-    <div className="content">
-      {isEdit ? (
-        <>
-          <textarea
-            ref={localContentInput}
-            value={localContent}
-            onChange={(e)=>setLocalContent(e.target.value)}
-          />
-        </>
-      ) : (
-        <>
-          {content}
-        </>
-      )}
-    </div>
+      <div className="info">
+        <span>
+          작성자 : {author} | 감정점수 : {emotion}
+        </span>
+        <br />
+        <span className="date">{new Date(created_date).toLocaleString()}</span>
+      </div>
+      <div className="content">
+        {isEdit ? (
+          <>
+            <textarea
+              ref={localContentInput}
+              value={localContent}
+              onChange={e => setLocalContent(e.target.value)}
+            />
+          </>
+        ) : (
+          <>{content}</>
+        )}
+      </div>
 
       {isEdit ? (
-      <>
-        <button onClick={handleQuitEdit}>수정 취소</button>
-        <button onClick={handleEdit}>수정 완료</button>
-      </>
+        <>
+          <button onClick={handleQuitEdit}>수정 취소</button>
+          <button onClick={handleEdit}>수정 완료</button>
+        </>
       ) : (
-      <>
-        <button onClick={handleRemove}>삭제하기</button>
-        <button onClick={toggleIsEdit}>수정하기</button>
-      </>
+        <>
+          <button onClick={handleRemove}>삭제하기</button>
+          <button onClick={toggleIsEdit}>수정하기</button>
+        </>
       )}
-  </div>
+    </div>
   );
 };
-export default DiaryItem;
+export default React.memo(DiaryItem);
